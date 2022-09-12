38414b8df7a3f219a019817e70d83a42df65cae7
Tyler-Henry <tmhenry3@asu.edu>
2022-09-11 17:58:42 -0700

    uhhhh

--- Assets/RollerBallPrefab/Dependencies/BallMovement.cs
+++ Assets/RollerBallPrefab/Dependencies/BallMovement.cs
@@ -2,29 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
     public float moveForce = 150f;
     public float maxSpeed = 10f;
     public float jumpForce = 150f;
-    public Slider slider;
 
-    public float boostForce = 200f;
-    public float boostMaxSpeed = 15f;
-    [Tooltip("Amount of seconds it takes boost to run out")]
-    public float boostSeconds = 5f;
-    [Tooltip("Amount of seconds it takes boost to replenish from empty")]
-    public float boostReplenishSeconds = 5f;
-    private float boostLeft;
-    private bool boostPressed = false;
-
-    private Vector3 initialPosition;
-    
-    
     private Vector3 _facing;
-    private Vector3 initialFacing;
     public Vector3 facing
     {
         get { return this._facing; }
@@ -38,65 +23,18 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initialPosition = transform.position;
-        boostLeft = boostSeconds;
         body = GetComponent<Rigidbody>();
         _facing = transform.forward;
-        initialFacing = _facing;
-    }
-
-    private float GetForce()
-    {
-        if (boostPressed && boostLeft > 0)
-            return boostForce;
-        return moveForce;
-    }
-
-    private float MaxSpeed()
-    {
-        if (boostPressed && boostLeft > 0)
-            return boostMaxSpeed;
-        return maxSpeed;
-    }
-
-    private void CheckIfFallen()
-    {
-        if(transform.position.y < -10)
-        {
-            ResetPlayer();
-        }
-    }
-
-    private void ResetPlayer()
-    {
-        transform.position = initialPosition;
-        _facing = initialFacing;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        CheckIfFallen();
-        if(boostPressed)
-        {
-            if (boostLeft > 0)
-                boostLeft -= Time.deltaTime;
-            else
-                boostLeft = 0;
-        }
-        else
-        {
-            if(boostLeft < boostSeconds)
-                boostLeft += Time.deltaTime * boostReplenishSeconds / boostSeconds;
-            if (boostLeft > boostSeconds)
-                boostLeft = boostSeconds;
-        }
-        slider.value = boostLeft / boostSeconds;
         var forwardComponent = facing * commandedDirection.z;
         var lateralComponent = Vector3.Cross(facing, Vector3.up) * -commandedDirection.x;
         var verticalComponent = new Vector3(0f, commandedDirection.y, 0f);
 
-        var force = (forwardComponent + lateralComponent + verticalComponent) * Time.deltaTime * GetForce();
+        var force = (forwardComponent + lateralComponent + verticalComponent) * Time.deltaTime * moveForce;
         body.AddForce(force);
 
         if (body.velocity.magnitude > 0.001)
@@ -109,17 +47,11 @@ public class BallMovement : MonoBehaviour
             _facing = transform.forward;
         }
         
-        if (body.velocity.magnitude > MaxSpeed())
-            body.velocity = body.velocity.normalized * MaxSpeed();
+        if (body.velocity.magnitude > maxSpeed)
+            body.velocity = body.velocity.normalized * maxSpeed;
 
         if (commandedDirection.y > 0)
             commandedDirection.y = 0;
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ResetPlayer();
-
-        }
     }
 
     public void Jump(InputAction.CallbackContext ctx)
@@ -131,19 +63,6 @@ public class BallMovement : MonoBehaviour
         }
     }
 
-    public void Boost(InputAction.CallbackContext ctx)
-    {
-        if(ctx.performed)
-        {
-            boostPressed = true;
-        }
-        if(ctx.canceled)
-        {
-            boostPressed = false;
-        }
-            
-    }
-
     public void Move(InputAction.CallbackContext ctx)
     {
         var movement = ctx.ReadValue<Vector2>();
@@ -153,11 +72,6 @@ public class BallMovement : MonoBehaviour
 
     public void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "Obstacle")
-        {
-            ResetPlayer();
-            
-        }
         Vector3 delta = Vector3.zero;
         List<ContactPoint> list = new List<ContactPoint>();
         col.GetContacts(list);
